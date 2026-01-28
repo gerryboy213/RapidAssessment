fc7f5163c7fde9e24b66d382690d433308fd9b29
Bryan <bryan.jarito@gmail.com>
2026-01-28 19:36:34 +0800

    macarthur

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app.config['MYSQL_DB'] = 'heeadsss_db'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  # Add this to fetch results as dictionaries
 
 # Database Configuration (Update your MySQL credentials)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/javier_form_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/macarthur_form_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
